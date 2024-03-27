e49c99e4a55129aaca45b1ffb4cbc3759ac274bd
Dan Vassallo <danielvassallo87@gmail.com>
2024-03-27 16:11:43 -0400

    Update entrypoint.sh

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh -l
 curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/aws-cloudformation/cloudformation-guard/main/install-guard.sh | sh
-
 export PATH="$HOME/.guard/bin:$PATH"
+exit_code=0
+if ! cfn-guard validate -r $1 -d $2 --output-format sarif  --show-summary none --structured > result.sarif; then
+  exit_code=1
+fi
 
-cfn-guard validate -r $1 -d $2 --output-format sarif  --show-summary none --structured > result.sarif
+if ! cfn-guard validate -r $1 -d $2 --output-format junit  --show-summary none --structured > result.xml; then
+  exit_code=1
+fi
 
-cfn-guard validate -r $1 -d $2 --output-format junit  --show-summary none --structured > result.xml
+exit $exit_code
