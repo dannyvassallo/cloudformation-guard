6270f434e69791448edd41373f58313e22fc4da2
Dan Vassallo <danielvassallo87@gmail.com>
2024-03-27 16:59:26 -0400

    fsdfsdafds

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/aws-cloudformation/cloudformation-guard/main/install-guard.sh | sh
 export PATH="$HOME/.guard/bin:$PATH"
-
+github_url="https://api.github.com/repos/$GITHUB_REPOSITORY/pulls/$GITHUB_HEAD_REF/comments"
 exit_code=0
 
 if ! cfn-guard validate -r $1 -d $2 --output-format sarif  --show-summary none --structured > result.sarif; then
@@ -14,12 +14,14 @@ if ! cfn-guard validate -r $1 -d $2 --output-format junit  --show-summary none -
   cat result.xml
 fi
 
+echo "$github_url"
+
 curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/$GITHUB_REPOSITORY/pulls/$GITHUB_HEAD_REF/comments \
+  "$github_url" \
   -d '{"body":"Great stuff!"}'
 
 exit $exit_code
