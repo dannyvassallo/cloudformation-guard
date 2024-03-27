439d83997f760da645a5b46cc6d074f1ac11bb8c
Dan Vassallo <danielvassallo87@gmail.com>
2024-03-27 16:52:01 -0400

    fdsfsdafdas

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -19,7 +19,7 @@ curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/$GITHUB_REPOSITORY/pulls/$GITHUB_BASE_REF/comments \
+  https://api.github.com/repos/$GITHUB_REPOSITORY/pulls/$GITHUB_EVENT_NAME/comments \
   -d '{"body":"Great stuff!"}'
 
 exit $exit_code
