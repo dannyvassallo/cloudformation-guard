c4adabc0c003965d40cc8776c577849a38f4c5be
Dan Vassallo <danielvassallo87@gmail.com>
2024-06-13 12:30:26 -0400

    more comments

--- pre_commit_hooks/cfn_guard.py
+++ pre_commit_hooks/cfn_guard.py
@@ -1,14 +1,13 @@
 import os
 import platform
 import shutil
+import subprocess
+import sys
 import tarfile
 import tempfile
-import argparse
 from typing import Sequence
 from urllib.parse import urlparse, urlencode
 from urllib.request import Request, urlopen
-import sys
-import subprocess
 
 LATEST_RELEASE_URL = "https://api.github.com/repos/aws-cloudformation/cloudformation-guard/releases/latest
 BIN_NAME = "cfn-guard"
@@ -43,46 +42,50 @@ def get_latest_tag() -> str:
 def get_binary_name() -> str:
   return BIN_NAME + (".exe" if current_os in windows_oses else "")
 
-# Install latest cfn-guard to the os tmp directory
+# Install latest cfn-guard to the OS tmp directory to avoid
+# global version conflicts with existing installations, rust,
+# and cargo
 def install_cfn_guard():
     latest_tag = get_latest_tag()
     binary_name = get_binary_name()
 
     if current_os in supported_oses:
-        url = release_urls_dict[current_os].replace("TAG", latest_tag)
-        filename = os.path.basename(urlparse(url).path)
+      url = release_urls_dict[current_os].replace("TAG", latest_tag)
+      filename = os.path.basename(urlparse(url).path)
 
-        with tempfile.TemporaryDirectory() as temp_dir:
-            req = request(url)
-            with urlopen(req) as response:
-                tar_path = os.path.join(temp_dir, filename)
-                with open(tar_path, "wb") as tar_file:
-                    tar_file.write(response.read())
+      # Create the temporary directory in which to store the guard binary
+      with tempfile.TemporaryDirectory() as temp_dir:
+        req = request(url)
+        with urlopen(req) as response:
+          tar_path = os.path.join(temp_dir, filename)
+          with open(tar_path, "wb") as tar_file:
+            tar_file.write(response.read())
 
-                with tarfile.open(tar_path, "r:gz") as tar:
-                    tar.extractall(temp_dir)
+          with tarfile.open(tar_path, "r:gz") as tar:
+            tar.extractall(temp_dir)
 
-            binary_path = None
-            for root, _, files in os.walk(temp_dir):
-                for file in files:
-                    if file == binary_name:
-                        binary_path = os.path.join(root, file)
-                        break
+        binary_path = None
+        for root, _, files in os.walk(temp_dir):
+          for file in files:
+            if file == binary_name:
+              binary_path = os.path.join(root, file)
+              break
 
-            if binary_path is None:
-                return
+        if binary_path is None:
+          return
 
-            tmp_dir = tempfile.gettempdir()
-            dest_path = os.path.join(tmp_dir, binary_name)
+        tmp_dir = tempfile.gettempdir()
+        dest_path = os.path.join(tmp_dir, binary_name)
 
-            os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+        os.makedirs(os.path.dirname(dest_path), exist_ok=True)
 
-            shutil.move(binary_path, dest_path)
+        shutil.move(binary_path, dest_path)
     else:
         # This is unlikely to happen though worth covering
         # in case the user attempts to use on another OS.
         raise Exception(UNSUPPORTED_OS_MSG)
 
+# Pass arguments to and run cfn-guard
 def run_cfn_guard(args: Sequence[str]) -> int:
     tmp_dir = tempfile.gettempdir()
     binary_name = get_binary_name()
@@ -99,9 +102,11 @@ def run_cfn_guard(args: Sequence[str]) -> int:
 
       return process.returncode
     else:
+        # Install cfn-guard if it doesn't exist and then run it.
         install_cfn_guard()
         return run_cfn_guard(args)
 
+# Entry point for the pre-commit hook
 def main(argv: Sequence[str] | None = None) -> int:
   # This only serves to chop the first arg (the filename) when running the script directly
   if argv is None:
@@ -109,5 +114,6 @@ def main(argv: Sequence[str] | None = None) -> int:
 
   return run_cfn_guard(argv)
 
+# Handle invocation from python directly
 if __name__ == '__main__':
     raise SystemExit(main())
