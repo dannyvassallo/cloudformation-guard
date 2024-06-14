9a3cce3c4e7a8df1eeeb79380235b8f76283d8e7
Dan Vassallo <danielvassallo87@gmail.com>
2024-06-14 11:17:02 -0400

    fix with black

--- pre_commit_hooks/cfn_guard.py
+++ pre_commit_hooks/cfn_guard.py
@@ -9,39 +9,46 @@ from typing import Sequence
 from urllib.parse import urlparse, urlencode
 from urllib.request import Request, urlopen
 
-LATEST_RELEASE_URL = "https://api.github.com/repos/aws-cloudformation/cloudformation-guard/releases/latest"
+LATEST_RELEASE_URL = (
+    "https://api.github.com/repos/aws-cloudformation/cloudformation-guard/releases/latest"
+)
 BIN_NAME = "cfn-guard"
 UNSUPPORTED_OS_MSG = "Unsupported operating system. Could not install cfn-guard."
 
 release_urls_dict = {
-  "darwin": "https://github.com/aws-cloudformation/cloudformation-guard/releases/download/TAG/cfn-guard-v3-macos-latest.tar.gz",
-  "linux": "https://github.com/aws-cloudformation/cloudformation-guard/releases/download/TAG/cfn-guard-v3-ubuntu-latest.tar.gz",
-  "win32": "https://github.com/aws-cloudformation/cloudformation-guard/releases/download/TAG/cfn-guard-v3-windows-latest.tar.gz",
-  "win64": "https://github.com/aws-cloudformation/cloudformation-guard/releases/download/TAG/cfn-guard-v3-windows-latest.tar.gz",
+    "darwin": "https://github.com/aws-cloudformation/cloudformation-guard/releases/download/TAG/cfn-guard-v3-macos-latest.tar.gz",
+    "linux": "https://github.com/aws-cloudformation/cloudformation-guard/releases/download/TAG/cfn-guard-v3-ubuntu-latest.tar.gz",
+    "win32": "https://github.com/aws-cloudformation/cloudformation-guard/releases/download/TAG/cfn-guard-v3-windows-latest.tar.gz",
+    "win64": "https://github.com/aws-cloudformation/cloudformation-guard/releases/download/TAG/cfn-guard-v3-windows-latest.tar.gz",
 }
 supported_oses = ["linux", "darwin", "win32", "win64"]
 windows_oses = ["win32", "win64"]
 current_os = platform.system().lower()
 install_dir = os.path.join(os.path.expanduser("~"), ".cfn-guard-pre-commit")
 
+
 # Roll our own get request method to avoid extra dependencies
 def request(url: str):
-  # Explicitly set the headers to avoid User-Agent "Python-urllib/x.y"
-  # https://docs.python.org/3/howto/urllib2.html#headers
-  return Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    # Explicitly set the headers to avoid User-Agent "Python-urllib/x.y"
+    # https://docs.python.org/3/howto/urllib2.html#headers
+    return Request(url, headers={"User-Agent": "Mozilla/5.0"})
+
 
 # Get the latest release tag from Github
 def get_latest_tag() -> str:
-  req = request(LATEST_RELEASE_URL)
+    req = request(LATEST_RELEASE_URL)
 
-  with urlopen(req) as response:
-    data = response.read().decode('utf-8')
-    import json
-    return json.loads(data)["tag_name"]
+    with urlopen(req) as response:
+        data = response.read().decode("utf-8")
+        import json
+
+        return json.loads(data)["tag_name"]
+
 
 # Get an OS specific binary name
 def get_binary_name() -> str:
-  return BIN_NAME + (".exe" if current_os in windows_oses else "")
+    return BIN_NAME + (".exe" if current_os in windows_oses else "")
+
 
 # Install the latest cfn-guard to the install_dir to avoid
 # global version conflicts with existing installations, rust,
@@ -62,26 +69,27 @@ def install_cfn_guard():
 
         # Extract tarball members to install_dir
         with tarfile.open(temp_file.name, "r:gz") as tar:
-          # Extract tarball members to install_dir
-          for member in tar.getmembers():
-            if member.isdir():
-              continue  # Skip directories
-            # Extract the filename from the full path within the archive
-            filename = os.path.basename(member.name)
-            # Join the install_dir path and the filename to get the full target path
-            file_path = os.path.join(install_dir, filename)
-            # Open the archived file
-            with tar.extractfile(member) as source:
-                # Create a new file using the file_path with write binary mode
-                with open(file_path, 'wb') as target:
-                  # Copy the contents of the archived file(s) to the target file
-                  shutil.copyfileobj(source, target)
+            # Extract tarball members to install_dir
+            for member in tar.getmembers():
+                if member.isdir():
+                    continue  # Skip directories
+                # Extract the filename from the full path within the archive
+                filename = os.path.basename(member.name)
+                # Join the install_dir path and the filename to get the full target path
+                file_path = os.path.join(install_dir, filename)
+                # Open the archived file
+                with tar.extractfile(member) as source:
+                    # Create a new file using the file_path with write binary mode
+                    with open(file_path, "wb") as target:
+                        # Copy the contents of the archived file(s) to the target file
+                        shutil.copyfileobj(source, target)
 
         binary_path = os.path.join(install_dir, binary_name)
         os.chmod(binary_path, 0o755)
         os.remove(temp_file.name)
     else:
         raise Exception(UNSUPPORTED_OS_MSG)
+
 
 # Pass arguments to and run cfn-guard
 def run_cfn_guard(args: Sequence[str]) -> int:
@@ -96,7 +104,7 @@ def run_cfn_guard(args: Sequence[str]) -> int:
         cmd = [f"cd {os.getcwd()} &&", binary_path] + list(args)
         project_root = os.path.dirname(os.path.abspath(__file__))
         try:
-            result = subprocess.run(' '.join(cmd), cwd=project_root, shell=True)
+            result = subprocess.run(" ".join(cmd), cwd=project_root, shell=True)
             return result.returncode
         except subprocess.CalledProcessError as e:
             return e.returncode
@@ -105,14 +113,16 @@ def run_cfn_guard(args: Sequence[str]) -> int:
         install_cfn_guard()
         return run_cfn_guard(args)
 
+
 # Entry point for the pre-commit hook
 def main(argv: Sequence[str] | None = None) -> int:
-  # This only serves to chop the first arg (the filename) when running the script directly
-  if argv is None:
-      argv = sys.argv[1:]
+    # This only serves to chop the first arg (the filename) when running the script directly
+    if argv is None:
+        argv = sys.argv[1:]
 
-  return run_cfn_guard(argv)
+    return run_cfn_guard(argv)
+
 
 # Handle invocation from python directly
-if __name__ == '__main__':
+if __name__ == "__main__":
     raise SystemExit(main())
--- tests/test_cfn_guard.py
+++ tests/test_cfn_guard.py
@@ -3,11 +3,24 @@ from __future__ import annotations
 from pre_commit_hooks.cfn_guard import main
 import os
 
+
 def test_failing_template():
-    ret = main(["validate", "--rules='./guard/resources/validate/rules-dir/'", "--data='./guard/resources/validate/data-dir/'"])
+    ret = main(
+        [
+            "validate",
+            "--rules='./guard/resources/validate/rules-dir/'",
+            "--data='./guard/resources/validate/data-dir/'",
+        ]
+    )
     assert ret == 19
 
 
 def test_passing_template():
-    ret = main(["validate", "--rules='./guard/resources/validate/rules-dir/s3_bucket_public_read_prohibited.guard'", "--data='./guard/resources/validate/data-dir/s3-public-read-prohibited-template-compliant.yaml'"])
+    ret = main(
+        [
+            "validate",
+            "--rules='./guard/resources/validate/rules-dir/s3_bucket_public_read_prohibited.guard'",
+            "--data='./guard/resources/validate/data-dir/s3-public-read-prohibited-template-compliant.yaml'",
+        ]
+    )
     assert ret == 0
