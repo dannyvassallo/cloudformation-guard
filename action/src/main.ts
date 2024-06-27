1c25a7d5fadcfd5fa902da473714f41c9b3fd34f
Dan Vassallo <danielvassallo87@gmail.com>
2024-06-27 15:02:21 -0400

    idk

--- action/src/main.ts
+++ action/src/main.ts
@@ -15,6 +15,7 @@ import { uploadCodeScan } from './uploadCodeScan';
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
+  console.warn('WHY DOESNT THIS WORK');
   core.info('Running action');
   debugLog(`Running action`);
   const { analyze, checkout } = getConfig();
